7be1fe82acc99ecc986bfac463d3bb488424e360
Matt Kissinger <matt.m.kissinger@gmail.com>
2025-09-22 19:36:49 -0400

    Fix base path for GitHub Pages deployment - use correct repository name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/terror-in-the-jungle/',
+  base: '/terror-in-the-jungle-vr-experimental/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
